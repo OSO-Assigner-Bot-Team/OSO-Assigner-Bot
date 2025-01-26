b2eec1b7bc125ac3ff6d9e6c0bd6e85fa7380873
ludrol <jot.s.gam@gmail.com>
2025-01-26 21:41:44 +0100

    Added Hello World! command

---
+++ commands/utility/hello.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+        .setName('hello')
+        .setDescription('Replies with World!'),
+
+    async execute(interaction) {
+        await interaction.reply('World!')
+}
+};        
