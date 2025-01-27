065378d32a7f8e049b6fdfebea2f226c9c485637
IdfbAn <48176725+IdfbAn@users.noreply.github.com>
2025-01-27 14:27:38 +0400

    Add /help command Fixes #4. Not very "efficient", as each command (including the "List of commands:" at the beginning) is its own message, but whatever.

---
+++ commands/utility/help.js
@@ -0,0 +1,29 @@
+const fs = require('node:fs');
+const { MessageFlags, SlashCommandBuilder, Collection } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('Gets a list of commands and what they do.'),
+
+	async execute(interaction) {
+		const commandFiles = fs.readdirSync(__dirname).filter(file => file.endsWith('.js'));
+
+		const commands = new Collection();
+
+		for (const file of commandFiles) {
+			const command = require(`./${file}`);
+			commands.set(command.data.name, command.data.description);
+			console.log(commands);
+		}
+
+		await interaction.reply({ content: 'List of commands:', flags: MessageFlags.Ephemeral });
+
+		for (const command of commands) {
+			await interaction.followUp({
+				content: `# /${command[0]}\n${command[1]}`,
+				flags: MessageFlags.Ephemeral,
+			});
+		}
+	},
+};
