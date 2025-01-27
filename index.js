ec9a15be4b62b8ffacff8eda5219706b21dbdf00
IdfbAn <48176725+IdfbAn@users.noreply.github.com>
2025-01-27 11:12:27 +0400

    index.js: Add back client.once

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ for (const folder of commandFolders) {
 	}
 }
 
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
