2309057483665d6c938775f160840a885f0a4c62
IdfbAn <48176725+IdfbAn@users.noreply.github.com>
2025-01-28 16:53:44 +0400

    Create /alljobs command At least for now, it just displays the CSV file itself.

---
+++ commands/utility/alljobs.js
@@ -0,0 +1,19 @@
+const fs = require('node:fs');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('alljobs')
+		.setDescription('Displays all jobs.')
+		.setDefaultMemberPermissions(PermissionFlagsBits.SendMessages |
+			PermissionFlagsBits.AttachFiles |
+			PermissionFlagsBits.ReadMessageHistory |
+			PermissionFlagsBits.AddReactions |
+			PermissionFlagsBits.UseApplicationCommands |
+			PermissionFlagsBits.SendPolls |
+			PermissionFlagsBits.ViewChannel),
+
+	async execute(interaction) {
+		interaction.reply(`\`${fs.readFileSync('jobs.csv')}\``);
+	},
+};
