19cbd6c5f3806fb953b3dd0b0530a49793377f18
IdfbAn <48176725+IdfbAn@users.noreply.github.com>
2025-01-28 19:07:16 +0400

    displayjob.js: Use for...of loop

--- commands/utility/displayjob.js
+++ commands/utility/displayjob.js
@@ -24,16 +24,16 @@ module.exports = {
 
 		const scene = parse(fs.readFileSync('jobs.csv'), { trim: true });
 
-		for (let i = 0; i < scene.length; i++) {
-			if (scene_id.toUpperCase() === scene[i][0]) {
+		for (const i of scene) {
+			if (scene_id.toUpperCase() === i[0]) {
 				interaction.reply(`
-* Scene ID: ${scene[i][0]}
-* Description: ${scene[i][1]}
-* Attachments: ${scene[i][2]}
-* Attributes: ${scene[i][3]}
-* Required roles: ${scene[i][4]}
-* Deadline: ${scene[i][5]}
-* Status: ${scene[i][6]}`);
+* Scene ID: ${i[0]}
+* Description: ${i[1]}
+* Attachments: ${i[2]}
+* Attributes: ${i[3]}
+* Required roles: ${i[4]}
+* Deadline: ${i[5]}
+* Status: ${i[6]}`);
 			}
 		}
 	},
