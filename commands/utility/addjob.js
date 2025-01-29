dc15b5735332ff7aa0435fe3eb57003281c7c391
IdfbAn <48176725+IdfbAn@users.noreply.github.com>
2025-01-29 14:13:09 +0400

    Remove space after commas in CSV file

--- commands/utility/addjob.js
+++ commands/utility/addjob.js
@@ -67,11 +67,11 @@ module.exports = {
 		const status = interaction.options.getString('status');
 
 		if (fs.existsSync('jobs.v0.csv')) {
-			fs.appendFileSync('jobs.v0.csv', `${scene_id}, ${description}, ${attachments}, ${attributes}, ${required_roles}, ${deadline}, ${status}\n`);
+			fs.appendFileSync('jobs.v0.csv', `${scene_id},${description},${attachments},${attributes},${required_roles},${deadline},${status}\n`);
 		}
 		else {
 			fs.appendFileSync('jobs.v0.csv', 'SceneId, Description, Attachments, Attributes, RequiredRoles, Deadline, Status\n');
-			fs.appendFileSync('jobs.v0.csv', `${scene_id}, ${description}, ${attachments}, ${attributes}, ${required_roles}, ${deadline}, ${status}\n`);
+			fs.appendFileSync('jobs.v0.csv', `${scene_id},${description},${attachments},${attributes},${required_roles},${deadline},${status}\n`);
 		}
 
 		interaction.reply(`
