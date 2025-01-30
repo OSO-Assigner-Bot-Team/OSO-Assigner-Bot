046e5753eb57cca4d04a483f18c11004477296a2
IdfbAn <48176725+IdfbAn@users.noreply.github.com>
2025-01-30 18:17:52 +0400

    /alljobs: Fix two things

--- commands/utility/alljobs.js
+++ commands/utility/alljobs.js
@@ -28,7 +28,7 @@ module.exports = {
 			}
 
 			// Create row
-			table = table.concat(`${i[0]}, "${i[1]}", ${i[2]}, ${i[3]}, ${i[4]}, ${i[5]}, ${i[6]}, ${i[7]}, ${i[7]}}\n`);
+			table = table.concat(`${i[0]}, "${i[1]}", ${i[2]}, ${i[3]}, ${i[4]}, ${i[5]}, ${i[6]}, ${i[7]}, ${i[8]}\n`);
 		}
 
 		interaction.reply(table);
