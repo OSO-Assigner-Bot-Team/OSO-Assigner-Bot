c49126ab40f2dc914d8aafe44104a66bf3b4e21b
ludrol <jot.s.gam@gmail.com>
2025-02-02 23:48:11 +0100

    help command better formatting

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,5 +1,7 @@
 const fs = require('node:fs');
 const { Collection, MessageFlags, SlashCommandBuilder } = require('discord.js');
+const HORIZONTAL_SPACE = 15;
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -19,7 +21,15 @@ module.exports = {
 		let msg = '# List of commands: ';
 
 		for (const command of commands) {
-			msg = msg.concat(`\n## /${command[0]}\n${command[1]}`);
+			// Make a spaces to pad the name so descriptions are aligned
+			let tab = '';
+			for (let i = 0;i < HORIZONTAL_SPACE - String(command[0]).length;i++) {
+				tab = tab + ' ';
+			};
+			// Combine the messages
+			msg = msg.concat(`\n \`/${command[0]}`);
+			msg = msg.concat(tab);
+			msg = msg.concat(`${command[1]}\``);
 		}
 
 		await interaction.reply({ content: msg, flags: MessageFlags.Ephemeral });
