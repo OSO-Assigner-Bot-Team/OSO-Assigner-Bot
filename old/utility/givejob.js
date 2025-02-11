7ca9dd8187ead1ad90b9bc1b11cb54dd89f1bbe3
ninjaninja140 <edenkneale@outlook.com>
2025-02-11 13:46:55 +0000

    Reorganise incorrect files

--- old/utility/givejob.js
+++ old/utility/givejob.js
@@ -1,7 +1,13 @@
 const fs = require('node:fs');
-const Job = require('../../src/jobsManager.js');
+const Job = require('../src/jobsManager.js');
 const { parse } = require('csv/sync');
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const {
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	SlashCommandBuilder,
+	PermissionFlagsBits,
+} = require('discord.js');
 
 const DATAFILE = 'jobs.v0.csv';
 
@@ -9,36 +15,28 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('givejob')
 		.setDescription('Assigns the chosen job to the chosen user.')
-		.addStringOption(option =>
-			option
-				.setName('scene_id')
-				.setDescription('Scene ID')
-				.setRequired(true))
-		.addUserOption(option =>
-			option
-				.setName('target')
-				.setDescription('User to assign job to')
-				.setRequired(true))
-		.setDefaultMemberPermissions(PermissionFlagsBits.SendMessages |
-			PermissionFlagsBits.AttachFiles |
-			PermissionFlagsBits.ReadMessageHistory |
-			PermissionFlagsBits.AddReactions |
-			PermissionFlagsBits.UseApplicationCommands |
-			PermissionFlagsBits.SendPolls |
-			PermissionFlagsBits.ViewChannel),
+		.addStringOption((option) => option.setName('scene_id').setDescription('Scene ID').setRequired(true))
+		.addUserOption((option) =>
+			option.setName('target').setDescription('User to assign job to').setRequired(true)
+		)
+		.setDefaultMemberPermissions(
+			PermissionFlagsBits.SendMessages |
+				PermissionFlagsBits.AttachFiles |
+				PermissionFlagsBits.ReadMessageHistory |
+				PermissionFlagsBits.AddReactions |
+				PermissionFlagsBits.UseApplicationCommands |
+				PermissionFlagsBits.SendPolls |
+				PermissionFlagsBits.ViewChannel
+		),
 
 	async execute(interaction) {
 		const pipe_job = new Job(interaction.options.getString('scene_id'));
 		const jobs = parse(fs.readFileSync(DATAFILE));
 		const member = interaction.options.getMember('target');
 
-		const stop = new ButtonBuilder()
-			.setCustomId('stop')
-			.setLabel('STOP')
-			.setStyle(ButtonStyle.Danger);
+		const stop = new ButtonBuilder().setCustomId('stop').setLabel('STOP').setStyle(ButtonStyle.Danger);
 
-		const row = new ActionRowBuilder()
-			.addComponents(stop);
+		const row = new ActionRowBuilder().addComponents(stop);
 
 		for (const i of jobs) {
 			if (pipe_job.getSceneId() === i[0]) {
@@ -62,7 +60,9 @@ If you find yourself unable to meet the deadline, or want to rescind working on 
 					withResponse: true,
 				});
 
-				interaction.reply(`The job for scene ${pipe_job.getSceneId()} has been assigned to ${member}.`);
+				interaction.reply(
+					`The job for scene ${pipe_job.getSceneId()} has been assigned to ${member}.`
+				);
 
 				const rescission = await message.awaitMessageComponent();
 
